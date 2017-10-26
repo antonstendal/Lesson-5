01b5255920797ae9892a565d1db83afcc975a961
Anton <antonstendal19@gmail.com>
2017-10-27 00:34:20 +0300

    First

---
+++ Script.js
@@ -0,0 +1,39 @@
+//опредиление координат элемента
+function getElementPosition(posOff)
+{
+    var elem = document.getElementById(posOff);
+ 
+    var w = elem.offsetWidth;
+    var h = elem.offsetHeight;
+ 
+    var l = 0;
+    var t = 0;
+ 
+    while (elem)
+    {
+        l += elem.offsetLeft;
+        t += elem.offsetTop;
+    }
+ 
+    return {"left":l, "top":t, "width": w, "height":h};
+}
+
+//‚вод цены в поле
+var x = document.getElementById('pay');
+function getChar(event) {
+      if (event.which == null) {
+        if (event.keyCode < 1000,00) return null;
+        return String.fromCharCode(event.keyCode) //возвращает строку
+      }
+
+      if (event.which != 0 && event.charCode != 0) {
+        if (event.which < 1000,00) return null;
+        return String.fromCharCode(event.which)
+      }
+
+      return null;
+    }
+	
+	
+	
+	
